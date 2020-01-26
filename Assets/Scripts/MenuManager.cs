cec7eba6b316a5e6c57c25b944b022706d2d0d75
Romain Roy <rroy.romain@gmail.com>
2020-01-26 17:51:05 +0100

    Fix menu branch merge

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Button buttonPlay;
+    [SerializeField] private UnityEngine.UI.Button buttonSettings;
+    [SerializeField] private UnityEngine.UI.Button buttonExit;
+    [SerializeField] private Settings settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonPlay.onClick.AddListener(Play);
+        buttonSettings.onClick.AddListener(ShowSettings);
+        buttonExit.onClick.AddListener(Exit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Play()
+    {
+        // Lancer le jeu
+    }
+
+    void ShowSettings()
+    {
+        settings.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+}
