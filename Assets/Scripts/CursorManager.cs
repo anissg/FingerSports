a1aae95d819f69b50dde327d5c4def51644aa03f
Anis <anis_sg@hotmail.com>
2020-02-03 16:39:21 +0100

    UI + ball physics + gameplay tweak

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
